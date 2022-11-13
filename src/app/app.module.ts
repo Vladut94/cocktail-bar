aa78ed8ed138730543b440fe67a93dedfea1618a
Vladut02 <vladut16.mihai94@gmail.com>
2022-11-13 13:02:43 +0200

    [create] components,interface and service for project and [define] routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AddCocktailComponent } from './components/add-cocktail/add-cocktail.component';
+import { CocktailsComponent } from './components/my-cocktails/cocktails/cocktails.component';
+import { CardCocktailComponent } from './components/my-cocktails/card-cocktail/card-cocktail.component';
+import { ModalCocktailComponent } from './components/my-cocktails/modal-cocktail/modal-cocktail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    AddCocktailComponent,
+    CocktailsComponent,
+    CardCocktailComponent,
+    ModalCocktailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/interfaces/cocktail.interface.ts
@@ -0,0 +1,9 @@
+export interface Cocktail {
+  id: number;
+  name: string;
+  author: string;
+  ingredients: string [];
+  description: string;
+  imageUrl: string;
+  withAlcohol: boolean;
+}
