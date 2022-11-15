549118201de5ef2800b85d5a3e0da339efab5f00
Vladut02 <vladut16.mihai94@gmail.com>
2022-11-15 03:13:56 +0200

    [create] a form in add-cocktail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
--- src/app/components/add-cocktail/add-cocktail.component.ts
+++ src/app/components/add-cocktail/add-cocktail.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-add-cocktail',
   templateUrl: './add-cocktail.component.html',
   styleUrls: ['./add-cocktail.component.css']
 })
+
+
 export class AddCocktailComponent implements OnInit {
+valueOfIngredients : [] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getIngredientsValue(){
+    console.log('nu merge');
+    // this.valueOfIngredients.push(event)
+  }
 }
