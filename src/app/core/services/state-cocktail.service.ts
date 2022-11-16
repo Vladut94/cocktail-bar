8a1ae0340cc302a3957240f2c91394853e034da3
Vladut02 <vladut16.mihai94@gmail.com>
2022-11-16 15:08:01 +0200

    [Delete] cocktails in card-cocktail component

--- src/app/core/services/state-cocktail.service.ts
+++ src/app/core/services/state-cocktail.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
 import {Cocktail} from "../interfaces/cocktail.interface";
 import {ApiCocktailService} from "./api-cocktail.service";
 
@@ -8,8 +8,19 @@ import {ApiCocktailService} from "./api-cocktail.service";
 })
 export class StateCocktailService {
    cocktails$ = new BehaviorSubject<Cocktail[]>([]);
+   private deleteCocktail$ = new Subject<number>();
 
   constructor(private apiCocktailService: ApiCocktailService) {
      this.apiCocktailService.getCocktails().subscribe((cocktails => this.cocktails$.next(cocktails)));
+
+     this.deleteCocktail$.subscribe((id: number) =>
+       this.cocktails$.next(this.cocktails$.getValue()
+         .filter((cocktail => {
+            return cocktail.id != id
+     }))));
+  }
+
+  deleteCocktail(id: number) {
+    this.deleteCocktail$.next(id)
   }
 }
